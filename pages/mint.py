e36e452a03ca668051bf09baf2022ea8fea2a6ba
Hursh Karkhanis <karkhanis.hursh@gmail.com>
2024-02-27 19:00:25 +0000

    good progress morning 2/27

--- pages/mint.py
+++ pages/mint.py
@@ -5,6 +5,8 @@ from pandasql import sqldf
 
 import pandasql as ps
 
+from datetime import datetime
+
 st.title("📗 Mint")
 
 url = "https://docs.google.com/spreadsheets/d/1n-hcvcfR4yMxqcolyOq2rBauGH1nFtCkWYYZgUgyEDs/edit?usp=sharing"
@@ -12,46 +14,54 @@ url = "https://docs.google.com/spreadsheets/d/1n-hcvcfR4yMxqcolyOq2rBauGH1nFtCkW
 conn = st.connection("gsheets", type=GSheetsConnection)
 
 # Read data from Google Sheets
-google_data = conn.read(spreadsheet=url, usecols=[0, 1, 2, 3])
+google_data = conn.read(spreadsheet=url, usecols=[0, 1, 2, 3, 4, 5])
 
-st.title("Google Sheet")
-st.write(google_data)
 
 pandas_data = pd.DataFrame(google_data)
 
-st.title("PandasDF")
-
-st.write(pandas_data)
-
-#====================
-
-st.title("asking user to filter")
-
-start_date = st.date_input("Select start date")
-end_date = st.date_input("Select end date")
-
-# Convert selected dates to the format in DataFrame
-start_date_str = start_date.strftime("%m/%d/%y")
-end_date_str = end_date.strftime("%m/%d/%y")
-
-print(start_date, type(start_date))
-print(end_date, type(end_date))
-
-print("-----")
+pandas_data['DATE'] = pd.to_datetime(pandas_data['DATE']).dt.date
 
 
-print(start_date_str, type(start_date_str))
+col1, col2 = st.columns(2)
 
-print(end_date_str, type(end_date_str))
+# Add date inputs to the columns
+with col1:
+    start_date = st.date_input("Start date")
+
+with col2:
+    end_date = st.date_input("End date")
+
+categories = st.multiselect("Select Categories", pandas_data['CATEGORY'].unique())
+
+formatted_start = start_date.strftime("%B %d, %Y")
+formatted_end = end_date.strftime("%B %d, %Y")
+
+st.subheader("🗓 " + formatted_start + " to " + formatted_end)
+if not categories:
+    st.subheader("🗂 All Categories")
+else:
+    subheader_text = " + ".join(categories)
+    st.subheader("🗂 Categories: " + subheader_text)
+
+# Convert date inputs to datetime objects
+start_datetime = datetime.combine(start_date, datetime.min.time())
+end_datetime = datetime.combine(end_date, datetime.max.time())
+
+if start_datetime <= end_datetime:
+    if not categories:
+        filtered_df = pandas_data[(pandas_data['DATE'] >= start_datetime.date()) & 
+                              (pandas_data['DATE'] <= end_datetime.date())]
+    else:
+        filtered_df = pandas_data[(pandas_data['DATE'] >= start_datetime.date()) & 
+                                (pandas_data['DATE'] <= end_datetime.date()) & 
+                                (pandas_data['CATEGORY'].isin(categories))]
+    st.write(filtered_df)
+
+else:
+    st.error("End date must be after start date.")
 
 
-# Filter dates between the time frame
 
-filtered_df = pandas_data[pandas_data['Date'] > '1/7/24']
-
-st.title("Filtered")
-
-st.write(filtered_df)
 
 
 
